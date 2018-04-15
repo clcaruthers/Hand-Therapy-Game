fe3bdab4344b40f13613d0fbeeef3e74f2e68bc8
Gustavo Lattari <gustavolattari@hotmail.com>
2018-04-15 14:35:48 -0400

    Added count for charging up

---
+++ Assets/TextBehavior.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TextBehavior : MonoBehaviour {
+
+    Text t;
+
+    GameObject manager;
+
+    void Awake()
+    {
+        t = this.GetComponent<Text>();
+
+        manager = GameObject.FindGameObjectWithTag("Manager");
+    }
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (this.transform.parent.gameObject.GetComponent<Image>().color.a < 1.0f)
+            t.text = "";
+        else
+        {
+            t.text = "x" + (manager.GetComponent<MotionDetection>().clenchCount + 1);
+        }
+        
+
+	}
+}
