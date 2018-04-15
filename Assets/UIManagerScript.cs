583312909fc805343384226834a22748a5a076b9
VZnet <cdungca@vidzone.net>
2018-04-15 13:53:32 -0400

    Menu stuff
    Main, credits, and license menus

---
+++ Assets/UIManagerScript.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class UIManagerScript : MonoBehaviour
+{
+    public void StartGame()
+    {
+        SceneManager.LoadScene("Scene1");
+    }
+
+    public void ViewCredits()
+    {
+        SceneManager.LoadScene("sceneCredits");
+    }
+
+    public void ViewMainMenu()
+    {
+        SceneManager.LoadScene("sceneMainMenu");
+    }
+
+    public void ViewLicenses()
+    {
+        SceneManager.LoadScene("sceneLicenses");
+    }
+}
