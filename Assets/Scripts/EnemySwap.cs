1d5001d66b53dc663aa67daeffe662944e0280fa
SYSTEMS\c4r15 <caris.frazier@gmail.com>
2018-05-03 01:18:35 -0400

    Array Loaded
    Elements are ready

--- Assets/Scripts/EnemySwap.cs
+++ Assets/Scripts/EnemySwap.cs
@@ -4,12 +4,7 @@ using UnityEngine;
 
 public class EnemySwap : MonoBehaviour {
 
-
-        public material;
-        public Material[] WaterMat;
-        public Material[] FireMat;
-        public Material[] EarthMat;
-        public Material[] LightningMat;
+        public Material[] TypeSwap;
+      
 
     }
-}
